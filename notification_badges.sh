cd8e9facd6b1b7480f36be254b4e2b33ef21c6ba
gregchi <35168758+gregchi@users.noreply.github.com>
2023-07-22 09:26:16 +0100

    Create notification_badges.sh
    Bash script which does install ubuntu-keyring in order to install libindicator7 from Ubuntu 20.04 LTS repository. It also install dash-to-dock extension and enables it. The outcome is getting notification badges for example on the dock icons like Thunderbird, Nautilus, etc. It's reasonably safe because there is no complex dendencies nad the Ubuntu repositories are removed after installation to avoid "Hell Of Dependencies".

---
+++ notification_badges.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or use sudo"
+  exit
+fi
+
+# Check if gnome-extensions is installed
+if ! command -v gnome-extensions &> /dev/null
+then
+    echo "gnome-extensions could not be found, installing..."
+    apt-get update
+    apt-get install -y gnome-extensions
+fi
+
+# Download ubuntu keyring package
+wget http://archive.ubuntu.com/ubuntu/pool/main/u/ubuntu-keyring/ubuntu-keyring_2021.03.26_all.deb
+
+# Install ubuntu keyring package
+dpkg -i ubuntu-keyring_2021.03.26_all.deb
+
+# Backup current sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+# Add Ubuntu 22.04 repositories to sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ focal main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse" >> /etc/apt/sources.list
+echo "deb http://archive.ubuntu.com/ubuntu/ focal-security main restricted universe multiverse" >> /etc/apt/sources.list
+
+# Update package list
+apt-get update
+
+# Install libindicator7 library
+apt-get install -y libindicator7
+
+# Restore original sources.list
+mv /etc/apt/sources.list.bak /etc/apt/sources.list
+
+# Update package list
+apt-get update
+
+# Download latest release of Dash to Dock
+wget $(curl -s https://api.github.com/repos/micheleg/dash-to-dock/releases/latest | grep 'browser_' | cut -d\" -f4)
+
+# Unzip the extension to the correct directory
+unzip dash-to-dock@micxgx.gmail.com.zip -d ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/
+
+# Enable the extension
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+
+# Print a message to reload the shell
+echo "Please reload the shell using 'Alt+F2 r Enter' to apply changes."
